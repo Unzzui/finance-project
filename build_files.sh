e9733145c05b2fd59288fb5020760af9b15cf3ce
unzzui <diegobravobe@gmail.com>
2024-01-04 20:17:04 -0300

    Update Python version and manage.py command

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python --version
+python3 --version
 pip3 install --upgrade pip
 pip3 install -r requirements.txt 
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
