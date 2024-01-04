8c134dcd22ab29b90ea821b0e10d54e375b52672
unzzui <diegobravobe@gmail.com>
2024-01-04 20:16:01 -0300

    Update database configuration in settings.py

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
+python --version
 pip3 install --upgrade pip
 pip3 install -r requirements.txt 
 python manage.py collectstatic --noinput
--- finance_project/settings.py
+++ finance_project/settings.py
@@ -75,14 +75,16 @@ WSGI_APPLICATION = "finance_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 CACHES = {
