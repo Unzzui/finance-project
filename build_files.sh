ceab5b7ccbbd12cc8376387a3dd3fc3c196bea93
unzzui <diegobravobe@gmail.com>
2024-01-04 19:45:25 -0300

    Update pip and Python versions

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
-pip install --upgrade pip
-pip install -r requirements.txt 
-python manage.py collectstatic
+pip3 install --upgrade pip
+pip3 install -r requirements.txt 
+python3 manage.py collectstatic
