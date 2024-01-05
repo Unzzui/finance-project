757d3d1ff31a387cb5942d6c7ee121062db86651
unzzui <diegobravobe@gmail.com>
2024-01-04 22:46:41 -0300

    Update ALLOWED_HOSTS in settings.py

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = "django-insecure-je@%5bfm%i()g@_=z)cin006$b_xb%d2wo+0qj00x&29*6x1)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 
 # Application definition
